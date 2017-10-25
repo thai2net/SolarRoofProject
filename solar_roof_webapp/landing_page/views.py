15dc0e059f19bc6f5271f2f3c801866bf3e89fba
Bank <thewatch8@gmail.com>
2017-10-26 00:50:37 +0700

    Build very simple landing pages.

--- solar_roof_webapp/landing_page/views.py
+++ solar_roof_webapp/landing_page/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'landing_page/index.html', context=None, content_type=None, status=None, using=None)
--- solar_roof_webapp/solar_roof_webapp/settings.py
+++ solar_roof_webapp/solar_roof_webapp/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'forecast_pv',
+    'landing_page',
+    'optimal_pv',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'solar_roof_webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
