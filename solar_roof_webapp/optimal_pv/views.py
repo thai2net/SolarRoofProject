cc52442efcf49c9ef1832769e5b0305330f037bc
Snowlaw <thewatch8@gmail.com>
2017-10-29 04:31:33 +0700

    Complete solar planner

--- solar_roof_webapp/optimal_pv/views.py
+++ solar_roof_webapp/optimal_pv/views.py
@@ -21,28 +21,39 @@ def page(request):
             '''
 
             energy = form.cleaned_data['bill']
+            max_pv_size = form.cleaned_data['houseSize']
+            max_pv_size = max_pv_size*0.12
 
-            data = pd.read_excel('dt13151011.xls')
-            load_sample = data.iloc[3:98, 3]
-            # load_sample = load_sample/np.max(load_sample)
-            energy_sample = np.sum(load_sample)/4
-
-            # scale load data to make energy equal to 1, so that if we have energy data we can sample this load profile
-            load_scale_energy = load_sample/energy_sample
-            scale = 0.6/1.4760870897413207
-            load_test = load_scale_energy*scale*energy
-            load_test = np.concatenate((load_test.reshape(95,1), np.array(load_test.iloc[-1]).reshape(1,1)))
-
+            load_test = import_load_data(energy)
             pv_15 = import_pv_data()
-            print('000000000000000000000000000000')
-            print(np.max(load_test))
-            pv_size = optimal_sizing(load_test, pv_15)
 
-            content_dict = {'title':'Solar Planner','form':form, 'bill':pv_size}
+            print('000000000000000000000000000000')
+            pv_size, bess_kWh, bess_kW, NPV, IRR = optimal_sizing(load_test, pv_15, max_pv_size)
+
+            if abs(pv_size) < 1e-2:
+                pv_size='0'
+
+            content_dict = {'title':'Solar Planner','form':form, 'pv_size':pv_size,
+            'bess_kWh':bess_kWh,'bess_kW':bess_kW,'NPV':NPV, 'IRR':IRR}
 
     return render(request, 'optimal_pv/optimal-page.html', context=content_dict, content_type=None, status=None, using=None)
 
-def optimal_sizing(load_test, pv_15):
+def import_load_data(energy):
+    data = pd.read_excel('dt13151011.xls')
+    load_sample = data.iloc[3:98, 3]
+
+    # Calculate basic energy in 1 month
+    energy_sample = np.sum(load_sample)/4*30
+
+    # load data scale monthly energy consumption equal to 1,
+    # so that if we have energy consumption in 1 month we can sample this load profile
+    load_scale_energy = load_sample/energy_sample
+    load_test = load_scale_energy*energy
+    load_test = np.concatenate((load_test.reshape(95,1), np.array(load_test.iloc[-1]).reshape(1,1)))
+
+    return load_test
+
+def optimal_sizing(load_test, pv_15, max_pv_size):
     pv_power_scale = pv_15/np.max(pv_15)
 
     off_peak_rate = 2.6296
@@ -61,7 +72,8 @@ def optimal_sizing(load_test, pv_15):
     cost_norm_pv = 37.5*1000        # Baht/W
 
     cost_pv = (cost_norm_inverter + cost_norm_pv)
-    cost_pv = cost_pv*5
+    # Scale PV cost for correction
+    cost_pv = cost_pv*10
 
     bess_cost_kWh = 300*35
     bess_cost_kW = (0.71 + 0.21 + 0.57 + 0.15 + 0.75 + 0.06)*35*1000
@@ -131,15 +143,20 @@ def optimal_sizing(load_test, pv_15):
     A_7 = np.array([0, -Crate, 1]).reshape(3,1)
     A_7 = np.concatenate((A_7, np.zeros((len_time,1))))
     A_7 = A_7.T
+    # Max PV
+    A_8 = np.array([1, 0, 0]).reshape(3,1)
+    A_8 = np.concatenate((A_8, np.zeros((len_time,1))))
+    A_8 = A_8.T
 
-    A = np.concatenate((A_1,A_2,A_3,A_4,A_5,A_6,A_7),axis=0)
+    A = np.concatenate((A_1,A_2,A_3,A_4,A_5,A_6,A_7, A_8),axis=0)
 
     b = load_test.reshape(96,1)
     b = np.concatenate((b, np.zeros((len_time*4,1))))
-    b = np.concatenate((b,np.array(0).reshape(1,1)))
-    b = np.concatenate((b,np.array(2).reshape(1,1)))
+    b = np.concatenate((b,np.array(0).reshape(1,1)))     # states of charge at start and end of the day must equal
+    b = np.concatenate((b,np.array(2).reshape(1,1)))     # Crate must less than or equal 2 (Commercialize Crate)
+    b = np.concatenate((b,np.array(max_pv_size).reshape(1,1)))
 
-    lb = np.array([[0, 0, 0], [10000, 10000, 10000]]).reshape(3,2)
+    lb = np.array([[0, 0, 0], [max_pv_size, 10000, 10000]]).reshape(3,2)
 
     for i in range(96):
         lb = np.concatenate((lb, np.array([-10000, 10000]).reshape(1,2)))
@@ -149,9 +166,33 @@ def optimal_sizing(load_test, pv_15):
     res = linprog(c, A_ub=list(A), b_ub=list(b), bounds=None)
     print('222222222222222222222222222222222222')
     pv_size = res['x'][0]
-    print(res)
+    bess_kWh = res['x'][1]
+    bess_kW = res['x'][2]
+    NPV_return = -res['fun']
+    X = res['x']
 
-    return pv_size
+    # IRR Calculation
+    NPV = NPV_calculator(X, r, year, pv_power_scale, tou, cost_pv, bess_cost_kWh, bess_cost_kW)
+    IRR = r
+    IRR_u = r*10
+    IRR_l = r*-15
+
+    print('11111111111111111111111111111111')
+    print(NPV)
+    IRR_temp = IRR
+
+    while abs(NPV) > 1e-1:
+
+        print(pv_size)
+        IRR_temp = (IRR_u + IRR_l)/2
+        NPV = NPV_calculator(X, IRR_temp, year, pv_power_scale, tou, cost_pv, bess_cost_kWh, bess_cost_kW)
+        if NPV > 0:
+            IRR_l = IRR_temp
+        else:
+            IRR_u = IRR_temp
+
+
+    return pv_size, bess_kWh, bess_kW, NPV_return, IRR_temp
 
 def import_pv_data():
     pv = pd.read_excel('./2013-12-22.xls')
@@ -172,3 +213,25 @@ def import_pv_data():
             count = 0
 
     return pv_15
+
+def NPV_calculator(x, r, year, pv_power_scale, tou, cost_pv, bess_cost_kWh, bess_cost_kW):
+    # NPV factor
+    year = 25
+    r = r
+    discount = (1 - (1/(1 + r/100))**year)/(1-(1/(1 + r/100)))
+
+    # coefficient of PV size
+    c = -np.dot(pv_power_scale, tou)
+    c = 365*c*discount + cost_pv
+    c = [c]
+
+    # coefficient of BESS energy
+    c.append(bess_cost_kWh)
+
+    # coefficient of BESS kW
+    c.append(bess_cost_kW)
+
+    # coefficient of BESS charge and discharge and TOU
+    c.extend(list(np.array(tou)*365*discount/4))
+
+    return -sum(c*x)
