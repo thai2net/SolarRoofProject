f52fea7537c35a5ccd5ae78fc85a928c6c4097f4
Bank <thewatch8@gmail.com>
2017-10-28 12:26:11 +0700

    Update Websites
    Signed-off-by: Bank <thewatch8@gmail.com>

--- solar_roof_webapp/static/javascript/basic_base.js
+++ solar_roof_webapp/static/javascript/basic_base.js
@@ -7,16 +7,17 @@ $(document).ready(function(){
       /* Check the location of each desired element */
       $('.fademe').each( function(i){
 
-          var bottom_of_object = $(this).position().top + $(this).outerHeight() - 500;
+          var bottom_of_object = $(this).position().top + $(this).outerHeight() - 450;
           var bottom_of_window = $(window).scrollTop() + $(window).height();
 
           /* If the object is completely visible in the window, fade it it */
           if( bottom_of_window > bottom_of_object ){
               console.log('Print t')
-              $(this).animate({'opacity':'1'},500);
+              $(this).animate({'opacity':'1'},1000);
 
           }
       });
   });
+
 
 })
